6049932b6479440b6f090b1fdfeb94bcc581556f
ZhuZiLiBryan <bryanzhuzili@gmail.com>
2022-04-21 16:57:00 -0700

    Added a test case to MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,33 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void TestOne() {
+        String finalContents = null;
+
+        try {
+            finalContents = Files.readString(Path.of("test-file.md"));
+        } catch(IOException e) {
+        }
+
+        ArrayList<String> links = MarkdownParse.getLinks(finalContents);
+
+        List<String> expected = List.of("https://something.com", "some-thing.html");
+
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), links.get(i));
+        }
     }
 }
